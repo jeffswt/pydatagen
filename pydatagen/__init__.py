424e0263222918dbeddcf93749687afa982f365b
Geoffrey, Tang <jeffswt@outlook.com>
2017-02-12 22:13:15 +0800

    Prepare to include updates.

---
+++ pydatagen/__init__.py
@@ -0,0 +1,34 @@
+
+"""
+pyDataGen:
+Python Implementation of Data Generators
+
+This project aims to give a full experience in generating OI data and random
+data in the most understood way and codes in the least bytes as possible, not
+regarding the generation speed.
+"""
+
+# __all__ = [
+#     # Imported functions
+#     'randint',
+#     'randrange',
+#     'choice',
+#     # Outputting functions
+#     'printf',
+#     'print_oi',
+#     'freopen',
+#     'fclose',
+#     # Self-defined randoming functions
+#     'possibility',
+#     'reseed',
+#     'randlist',
+#     'randlist2d',
+#     # Self-defined classes
+#     'UnionFindSet',
+#     'Graph',
+#     'Tree'
+# ]
+
+import sys
+import random
+
